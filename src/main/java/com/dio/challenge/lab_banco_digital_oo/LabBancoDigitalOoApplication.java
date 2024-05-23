1691956c9d82d6c8e46ebe688914b4c7fd659f5b
ThiagoMdO <thiago_catureba@hotmail.com>
2024-05-23 10:40:19 -0300

    First commit

---
+++ src/main/java/com/dio/challenge/lab_banco_digital_oo/LabBancoDigitalOoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.challenge.lab_banco_digital_oo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabBancoDigitalOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabBancoDigitalOoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/challenge/lab_banco_digital_oo/LabBancoDigitalOoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.challenge.lab_banco_digital_oo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabBancoDigitalOoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
